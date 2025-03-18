d8f826488ad20a58672521de5eeb297964a13c1e
Jais Andersen <jais.h.sorensen@gmail.com>
2025-03-18 10:21:36 +0100

    Added methods

--- index.js
+++ index.js
@@ -3,10 +3,36 @@ Vue.createApp({
         return{
             number1: 0,
             number2: 0,
-            result: 0
+            result: 0,
+            operator: "+"
         }
     },
     methods: {
-
+        Calculate() {
+            if(this.operator == "+"){
+            this.add()
+            }
+            else if(this.operator == "-"){
+            this.subtract()
+            }
+            else if(this.operator == "*"){
+            this.multiply()
+            }
+            else if(this.operator == "/"){
+            this.divide()
+            }
+        },
+        add(){
+            this.result = this.number1 + this.number2
+        },
+        subtract(){
+            this.result = this.number1 - this.number2
+        },
+        multiply(){
+            this.result = this.number1 * this.number2
+        },
+        divide(){
+            this.result = this.number1 / this.number2
+        }
     }
 }).mount('#app')
